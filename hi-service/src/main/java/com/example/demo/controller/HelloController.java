237907befd386237db27b85110233dbb36f5b762
shaomin.lin <shaomin.lin@qishon.com>
2018-10-25 17:22:55 +0800

    负载均衡

--- hi-service/src/main/java/com/example/demo/controller/HelloController.java
+++ hi-service/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,8 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
-    @RequestMapping(value = "/hello")
-    public String say() {
-        return "Hello Spring Boot!";
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+        return "hi " + name + " ,i am from port:" + port;
     }
 }
